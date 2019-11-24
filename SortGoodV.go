aef828144613d2ce01562ef0c112643cb2727e26
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2019-11-24 21:19:54 +0300

    Add files via upload

---
+++ SortGoodV.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"time"
+)
+
+type Interface interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(I, j int)
+	}
+
+type Person struct {
+	firstName string
+	lastName  string
+	birthDay  time.Time
+	}
+
+type People []Person
+
+func (p People) Len() int {
+	return len(p)
+	}
+func (p People) Less(i, j int) bool {
+	if p[i].firstName < p[j].firstName {
+		return true
+	}
+	if p[i].firstName > p[j].firstName {
+		return false
+	}
+	if p[i].lastName < p[j].lastName {
+		return true
+	}
+	if p[i].lastName > p[j].lastName {
+		return false
+	}
+	return p[i].birthDay.Sub(p[j].birthDay) > 0
+	}
+func (p People) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+	}
+
+func main() {
+	ivanIvanovDate, err := time.Parse("2006-Jan-02", "2005-Aug-10")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ivanIvanov2Date, err := time.Parse("2006-Jan-02", "2003-Aug-05")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	artemIvanovDate, err := time.Parse("2006-Jan-02", "2005-Aug-10")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p := People{
+		{"Ivan", "Ivanov", ivanIvanovDate},
+		{"Ivan", "Ivanov", ivanIvanov2Date},
+		{"Artem", "Ivanov", artemIvanovDate},
+	}
+	sort.Sort(p)
+	for _, o := range p {
+		fmt.Println(o.firstName, o.lastName, o.birthDay)
+	}
+}
