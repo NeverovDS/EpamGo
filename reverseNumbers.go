29c500b206f0a2d4ddfc48935b93c87c65be1b27
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2019-11-25 22:30:53 +0300

    Add files via upload

--- reverseNumbers.go
+++ reverseNumbers.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-func max(sl []int64) []int64 {
-	k := make([]int64,0)
+func reverse(sl []int64) []int64 {
+	k := make([]int64, 0)
 
 	for i, j := int64(0), int64(len(sl)-1); i < j; i, j = i+1, j-1 {
 		sl[i], sl[j] = sl[j], sl[i]
@@ -14,7 +14,7 @@ func max(sl []int64) []int64 {
 	return k
 }
 func main() {
-	s := []int64{1, 2, 5, 15,325,21,523,14,16,24,0}
-	d := max(s)
+	s := []int64{1, 2, 5, 15, 325, 21, 523, 14, 16, 24, 0}
+	d := reverse(s)
 	fmt.Println(d)
 }
