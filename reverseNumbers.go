450194ae0ee2ac13b9c7a8d0272a613c613b935b
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2019-11-28 02:42:29 +0300

    Add files via upload

--- reverseNumbers.go
+++ reverseNumbers.go
@@ -9,10 +9,12 @@ func reverseSlice(slice []int64) []int64 {
 
 	for i, j := int64(0), int64(len(slice)-1); i < j; i, j = i+1, j-1 {
 		slice[i], slice[j] = slice[j], slice[i]
-		result = append(slice)
+		result = slice
+
 	}
 	return result
 }
+
 func main() {
 	input := []int64{1, 2, 5, 15, 325, 21, 523, 14, 16, 24, 0}
 	output := reverseSlice(input)
