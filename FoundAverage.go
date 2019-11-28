6689522f74eb77ff962ec982dcfad26628f23c26
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2019-11-28 17:09:16 +0300

    Add files via upload

--- FoundAverage.go
+++ FoundAverage.go
@@ -7,7 +7,9 @@ import (
 func average(arr [] int) float64 {
 	sum := 0
 	n := len(arr)
-	if len(arr) == 0 {fmt.Println("Error")}
+	if len(arr) == 0 {
+		return 0
+	}
 	if len(arr) != 0 {
 
 		for i := 0; i < len(arr); i++ {
